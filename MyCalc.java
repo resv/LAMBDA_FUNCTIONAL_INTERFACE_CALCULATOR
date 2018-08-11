68007f10cb491cc8f66dce409cc9ba16b215bd24
Adam Kim <akim456@gmail.com>
2018-08-11 19:53:59 -0400

    x

--- MyCalc.java
+++ MyCalc.java
@@ -2,17 +2,17 @@ package inClassAnonClass.LAMBDA_FI_CALCULATOR;
 
 public class MyCalc {
 
-	Calculator<Integer> sum = (num1, num2)-> {
+	public Calculator<Integer> sum = (num1, num2)-> {
 		int result = (num1+num2);
 		return result;
 	};
 	
-	Calculator<Integer> subtract = (num1,num2)->{
+	public Calculator<Integer> subtract = (num1,num2)->{
 		int result = (num1-num2);
 		return result;
 	};
 	
-	Calculator<Float> multiply = (num1,num2)->{
+	public Calculator<Float> multiply = (num1,num2)->{
 		Float result = (num1*num2);
 		return result;
 	};
